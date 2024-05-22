74d0cb4bfa46cca3e9cb3edc04a294cacaad2150
Carol-mMartins <155315057+Carol-mMartins@users.noreply.github.com>
2024-05-22 16:18:26 -0300

    Alura-books--projeto-inicial-comJs

---
+++ app/metodoSort.js
@@ -0,0 +1,6 @@
+let btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco').addEventListener('click', ordenarLivrosPorPreco)
+
+function ordenarLivrosPorPreco() {
+    let livrosOrdenados = livros.sort((a,b) => a.preco - b.preco)
+    exibirOsLivrosNaTela (livrosOrdenados)
+}
